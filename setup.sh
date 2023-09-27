53aae7a3f9a0b617104c402b0ed2e290f1764a4d
Diogo Martins <60630955+diromm@users.noreply.github.com>
2023-09-27 14:22:15 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,35 +1,35 @@
-podman pull fjardim/mds-airflow
-podman pull fjardim/mds-kafka-zookeeper
-podman pull fjardim/mds-kafka-broker
-podman pull fjardim/mds-kafka-schema-registry
-podman pull fjardim/mds-kafka-connect
-podman pull fjardim/mds-kafka-control-center
-podman pull fjardim/mds-kafka-ksqldb-server
-podman pull fjardim/mds-kafka-sqldb-cli
-podman pull fjardim/mds-kafka-rest
-podman pull fjardim/mds-nifi
-podman pull fjardim/mds-postgres
-podman pull fjardim/mds-redis
-podman pull fjardim/mds-spark
-podman pull fjardim/mds-metabase
-podman pull fjardim/mds-elasticsearch
-podman pull fjardim/mds-kibana
-podman pull fjardim/mds-trino 
-podman pull fjardim/mds-hive
-podman pull fjardim/mds-hive-metastore
-podman pull fjardim/mds-minio
-podman pull fjardim/mds-superset
-podman pull fjardim/mds-pinot-zookeeper
-podman pull fjardim/mds-pinot
-podman pull fjardim/mds-namenode
-podman pull fjardim/mds-datanode
-podman pull fjardim/mds-hue
-podman pull mongo
-podman pull mongo-express
-podman pull cassandra
-podman pull mysql
-podman pull adminer
+docker pull fjardim/mds-airflow
+docker pull fjardim/mds-kafka-zookeeper
+docker pull fjardim/mds-kafka-broker
+docker pull fjardim/mds-kafka-schema-registry
+docker pull fjardim/mds-kafka-connect
+docker pull fjardim/mds-kafka-control-center
+docker pull fjardim/mds-kafka-ksqldb-server
+docker pull fjardim/mds-kafka-sqldb-cli
+docker pull fjardim/mds-kafka-rest
+docker pull fjardim/mds-nifi
+docker pull fjardim/mds-postgres
+docker pull fjardim/mds-redis
+docker pull fjardim/mds-spark
+docker pull fjardim/mds-metabase
+docker pull fjardim/mds-elasticsearch
+docker pull fjardim/mds-kibana
+docker pull fjardim/mds-trino 
+docker pull fjardim/mds-hive
+docker pull fjardim/mds-hive-metastore
+docker pull fjardim/mds-minio
+docker pull fjardim/mds-superset
+docker pull fjardim/mds-pinot-zookeeper
+docker pull fjardim/mds-pinot
+docker pull fjardim/mds-namenode
+docker pull fjardim/mds-datanode
+docker pull fjardim/mds-hue
+docker pull mongo
+docker pull mongo-express
+docker pull cassandra
+docker pull mysql
+docker pull adminer
 
 docker-compose up -d postgres
 
-podman exec -it postgres bash /backup/script.sh
+docker exec -it postgres bash /backup/script.sh
